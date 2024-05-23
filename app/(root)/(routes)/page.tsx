d3d75dd3b912d1b4f44ca4d595976e59e0d09d80
Hari Charan <hariRagu2831@gmail.com>
2024-05-23 19:48:10 +0530

    light and dark mode setup done

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,13 +1,15 @@
+import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="bg-red-500">
+    <div className="">
      <UserButton
       afterSignOutUrl="/"
      />
+     <ModeToggle />
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const inter = Open_Sans({ subsets: ["latin"] });
 
@@ -16,10 +18,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-        <ClerkProvider>
-   <html lang="en">
-        <body>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(
+          inter.className,
+          'bg- white dark:bg-[#313338]'
+        )}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            storageKey="discord-theme"
+          >
             {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
