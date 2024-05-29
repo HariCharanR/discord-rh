ceb8e17a8fe8665e07fb68d92e45c413ff1dfdad
Hari Charan <hariRagu2831@gmail.com>
2024-05-29 19:31:34 +0530

    Schema Updated Sucessfully

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,5 +1,6 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
+import { ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
 
 interface ServerSidebarProps {
@@ -24,15 +25,37 @@ const ServerSidebar = async ({
         include : {
             channels : {
                 orderBy : {
-                    
+                    createdAt : 'asc'
+                }
+            },
+            members : {
+                include : {
+                    profile : true,
+                },
+                orderBy : {
+                    role : 'asc'
                 }
             }
         }
-    })
+    });
+
+    const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT);
+
+    const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO);
+
+    const videoChannels = server?.channels.filter((channel) => channel.type === ChannelType.VIDEO);
+
+    const members = server?.members.filter((member) => member.profileId !== profile.id)
+
+    if(!server){
+        return redirect("/");
+    }    
+
+    const ourRole = server.members.find((members) => members.profileId === profile.id)?.role
 
     return ( 
-        <div className="">
-                Server Sidebar Component
+        <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5] ">
+               {/* <ServerHeader server={server} role={role} /> */}
         </div>
      );
 }
